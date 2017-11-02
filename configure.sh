ddd27cf6f1c948a838540bbf6542aaf4e1a5f6ae
Edward Schaefer <edwardaschaefer@gmail.com>
2017-11-01 23:12:08 -0400

    Added installation of most apt-get (non-pip) packages

--- configure.sh
+++ configure.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#configure ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
@@ -6,3 +7,24 @@ sudo ufw allow 2200/tcp
 sudo ufw allow www
 sudo ufw allow ntp
 sudo ufw enable
+
+#Update packages:
+sudo apt-get update -y
+#Install python 2.7:
+sudo apt-get install python2.7 -y
+#Install pip:
+sudo apt-get install python-pip -y
+#Install apache2:
+sudo apt-get install apache2 -y
+sudo apt-get install apache2-dev -y
+#Install python wsgi dependencies:
+sudo apt-get install python-setuptools -y
+sudo apt-get install libapache2-mod-wsgi -y
+#restart apache2:
+sudo service apache2 restart
+#Install sqlalchemy
+sudo apt-get install python-sqlalchemy -y
+#Install psycopg2:
+sudo apt-get install python-psycopg2 -y
+#Install PostgreSQL:
+sudo apt-get install postgresql -y
